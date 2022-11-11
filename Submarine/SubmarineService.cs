9838ceb56eabdf378e08b0210a561c242680bbfb
Tim Norris <tim.norris@smartsurjoutlook.onmicrosoft.com>
2022-11-11 17:15:00 -0500

    Test Compiling - Service Not Implemented

---
+++ Submarine/SubmarineService.cs
@@ -0,0 +1,8 @@
+﻿namespace Submarine;
+public static class SubmarineService
+{
+    public static Position CalculatePosition(string commands)
+    {
+        throw new NotImplementedException();
+    }
+}
